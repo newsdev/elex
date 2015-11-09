f5313ed784686198bf895339850954483d9f0772
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-11-09 10:26:12 -0500

    Version bump for refactored API.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name='nyt-ap-elections',
-    version='0.0.30',
+    version='0.0.31',
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/nyt-ap-elections',
