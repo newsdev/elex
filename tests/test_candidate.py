31ef266b05159b37908afd25525baa79b7d00dd7
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-11-10 14:28:01 -0500

    Tests candidates.

---
+++ tests/test_candidate.py
@@ -0,0 +1,21 @@
+import tests
+
+class TestCandidate(tests.ElectionResultsTestCase):
+
+    def test_number_of_candidates(self):
+        self.assertEqual(len(self.candidates), 3)
+
+    def test_candidate_object_inflation(self):
+        c = self.candidates[0]
+        self.assertEqual(type(c).__name__, 'Candidate')
+        self.assertEqual(c.__module__, 'elex.parser.api')
+
+    def test_candidate_attributes(self):
+        c = self.candidates[2]
+        self.assertEqual(c.first, 'Matt')
+        self.assertEqual(c.last, 'Bevin')
+        self.assertEqual(c.party, "GOP")
+        self.assertEqual(c.candidateid, "5295")
+        self.assertEqual(c.polid, "63424")
+        self.assertEqual(c.ballotorder, 2)
+        self.assertEqual(c.polnum, "20103")
