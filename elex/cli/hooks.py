66f78bd5b9632b20a57e72dcc03263ce329bb7ee
David Eads <davideads@gmail.com>
2015-11-10 14:37:30 -0500

    pretty text

--- elex/cli/hooks.py
+++ elex/cli/hooks.py
@@ -1,4 +1,4 @@
-from clint.textui import puts
+from clint.textui import puts, colored
 from elex.parser import api
 from elex.cli.utils import parse_date
 
@@ -12,9 +12,10 @@ def process_date_hook(app):
             app.argv[-1] = parse_date(app.argv[-1])
             return
         except ValueError:
-            puts('"{0}" could not be recognized as a date.\n'.format(app.argv[-1]))
+            puts(colored.green('Whoa there, friend! There was an error:\n'))
+            puts('{0} could not be recognized as a date.\n'.format(colored.yellow(app.argv[-1])))
     else:
-        puts('Please specify an election date and optional command (e.g. `elex init-races 2015-11-03`)\n')
+        puts(colored.yellow('Please specify a command and election date (e.g. `elex init-races 2015-11-03`). See below for details.\n'))
 
     app.args.print_help()
     app.close()
