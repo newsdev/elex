7f4d9c9358adffa83432d1af2cca0656ec73f07c
David Eads <davideads@gmail.com>
2015-11-16 11:37:39 -0500

    swap for dynamic tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,89 +1,46 @@
 import csv
 import sys
+import tests
 
 from cStringIO import StringIO
-from cement.utils import test
 from elex.cli.app import ElexApp
 
 DATA_FILE = 'tests/data/20151103_national.json'
 ELECTIONS_DATA_FILE = 'tests/data/00000000_elections.json'
+TEST_COMMANDS = ['races', 'candidates', 'reporting-units', 'candidate-reporting-units']
 
 
-class ElexCLITestCase(test.CementTestCase):
-    app_class = ElexApp
+class ElexCLITestMeta(type):
+    def __new__(mcs, name, bases, dict):
 
-    def setUp(self):
-        """
-        Reassign stdout for capture
-        """
-        self.stdout_backup = sys.stdout
-        sys.stdout = StringIO()
+        def gen_test(command, subcommand):
+            """
+            Dynamically generate a test function, like test_init_races
+            """
+            def test(self):
+                cli_fields, cli_data = self._test_command(argv=[subcommand])
+                api_data = getattr(self, command.replace('-', '_'))
+                api_fields = api_data[0].serialize().keys()
+                self.assertEqual(cli_fields, api_fields)
+                self.assertEqual(len(cli_data), len(api_data))
+                for i, row in enumerate(cli_data):
+                    for k, v in api_data[i].serialize().items():
+                        if v is None:
+                            v = ''
+                        self.assertEqual(row[k], str(v))
 
-    def tearDown(self):
-        """
-        Restore stdout
-        """
-        sys.stdout.close()
-        sys.stdout = self.stdout_backup
+            return test
 
-    def test_init_races_fields(self):
-        """
-        Test `elex init-races` field names
-        """
-        fields, data = self._test_command(argv=['init-races'])
-        self.assertEqual(fields, ['raceid', 'racetype', 'racetypeid', 'description', 'initialization_data', 'lastupdated', 'national', 'officeid', 'officename', 'party', 'seatname', 'seatnum', 'statename', 'statepostal', 'test', 'uncontested'])
+        for command in TEST_COMMANDS:
+            subcommand = 'init-{0}'.format(command)
+            test_name = 'test_{0}'.format(subcommand.replace('-', '_'))
+            dict[test_name] = gen_test(command, subcommand)
 
-    def test_init_races_data(self):
-        """
-        Test `elex init-races` data
-        """
-        fields, races = self._test_command(argv=['init-races'])
-        race = races[-1]
-        self.assertEqual(race['national'], 'True')
-        self.assertEqual(race['officeid'], 'G')
-        self.assertEqual(race['officename'], 'Governor')
-        self.assertEqual(race['raceid'], '18525')
-        self.assertEqual(race['racetype'], 'General')
-        self.assertEqual(race['racetypeid'], 'G')
+        return type.__new__(mcs, name, bases, dict)
 
-    def test_init_candidates_fields(self):
-        """
-        Test `elex init-candidates` field names
-        """
-        fields, data = self._test_command(argv=['init-candidates'])
-        self.assertEqual(fields, ['unique_id', 'candidateid', 'ballotorder', 'first', 'last', 'party', 'polid', 'polnum'])
 
-    def test_init_candidates_data(self):
-        """
-        Test `elex init-candidates` data
-        """
-        fields, candidates = self._test_command(argv=['init-candidates'])
-        candidate = candidates[-1]
-        self.assertEqual(candidate['ballotorder'], '2')
-        self.assertEqual(candidate['candidateid'], '5295')
-        self.assertEqual(candidate['first'], 'Matt')
-        self.assertEqual(candidate['last'], 'Bevin')
-        self.assertEqual(candidate['party'], 'GOP')
-        self.assertEqual(candidate['polid'], '63424')
-        self.assertEqual(candidate['polnum'], '20103')
-        self.assertEqual(candidate['unique_id'], 'polid-63424')
-
-    def test_elections_fields(self):
-        """
-        Test `elex elections` field names
-        """
-        fields, data = self._test_command(argv=['elections'], datafile=ELECTIONS_DATA_FILE)
-        self.assertEqual(fields, ['electiondate', 'liveresults', 'testresults'])
-
-    def test_elections_data(self):
-        """
-        Test `elex elections` data
-        """
-        fields, data = self._test_command(argv=['elections'], datafile=ELECTIONS_DATA_FILE)
-        election = data[-1]
-        self.assertEqual(election['electiondate'], '2016-02-09')
-        self.assertEqual(election['liveresults'], 'False')
-        self.assertEqual(election['testresults'], 'True')
+class ElexCLITestCase(tests.ElectionResultsTestCase):
+    __metaclass__ = ElexCLITestMeta
 
     def test_next_election_fields(self):
         """
@@ -104,10 +61,17 @@ class ElexCLITestCase(test.CementTestCase):
         """
         Execute an `elex` sub-command; returns fieldnames and rows
         """
+        stdout_backup = sys.stdout
+        sys.stdout = StringIO()
+
         app = ElexApp(argv=argv + ['--data-file', datafile])
         app.setup()
         app.log.set_level('FATAL')
         app.run()
         lines = sys.stdout.getvalue().split('\n')
         reader = csv.DictReader(lines)
+
+        sys.stdout.close()
+        sys.stdout = stdout_backup
+
         return reader.fieldnames, list(reader)
