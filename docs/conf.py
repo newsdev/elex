3cbd96fe348472b6168d815812be40f6ad22efda
David Eads <davideads@gmail.com>
2015-11-19 01:52:12 -0500

    sketch out docs

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import elex
+import os
+import sys
+
+# Path munging
+sys.path.insert(0, os.path.abspath('..'))
+
+# Extensions
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+autodoc_member_order = 'bysource'
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/3.5', None)
+}
+
+# Templates
+templates_path = ['_templates']
+master_doc = 'index'
+
+# Metadata
+project = u'nyt-ap-elections'
+copyright = u'2015, New York Times & NPR'
+version = elex.__version__
+release = elex.__version__
+
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+
+# HTMl theming
+html_theme = 'default'
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
+htmlhelp_basename = 'agatedoc'
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+==========================
+nyt-ap-elections |release|
+==========================
+
+Load election results data from the AP version 2.0 API.
+
+
+Features
+---------------------
+
+* Uses v2.0 of the Associated Press Election API.
+* Intuitive command line interface: Get data as CSV or JSON and pipe to the data tool of your choice. 
+* Friendly Python API for use as a library.
+* Simple election recording (to MongoDB).
+* Comprehensive tests.
+* Extensive documentation.
+* Decently fast (several times faster than previous efforts at NYT and NPR).
+
+.. include:: ../README.rst
