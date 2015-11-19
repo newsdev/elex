0db899176c1d0a18e6a6fe02b149830224ad3a5b
David Eads <davideads@gmail.com>
2015-11-19 01:52:01 -0500

    minor refactor of version string declaration

--- elex/__init__.py
+++ elex/__init__.py
@@ -1,5 +1,8 @@
 import os
+import pkg_resources
 
+
+__version__ = pkg_resources.get_distribution('nyt-ap-elections').version
 VERSION = os.environ.get('AP_API_VERSION', 'v2')
 BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/%s/elections' % VERSION)
 API_KEY = os.environ.get('AP_API_KEY', None)
