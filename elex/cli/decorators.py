6546a06d79be212c16904be5ffdb3fa79d797077
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-12-03 10:52:26 -0500

    Bugfixing.

--- elex/cli/decorators.py
+++ elex/cli/decorators.py
@@ -8,15 +8,17 @@ def require_date(fn):
     def decorated(self):
         name = fn.__name__.replace('_', '-')
         if not self.app.pargs.data_file and len(self.app.pargs.date) and self.app.pargs.date[0]:
-            try:
-                self.app.election.electiondate = parse_date(self.app.pargs.date[0])
-                return fn(self)
-            except ValueError:
-                puts(colored.yellow('Whoa there, friend! There was an error:\n'))
-                puts('{0} could not be recognized as a date.\n'.format(colored.green(self.app.pargs.date[0])))
-            except KeyError:
-                puts(colored.yellow('Whoa there, friend! There was an error:\n'))
-                puts('You have not exported {0} as an environment variable.\n'.format(colored.green("AP_API_KEY")))
+            self.app.election.electiondate = parse_date(self.app.pargs.date[0])
+            return fn(self)
+            # try:
+            #     self.app.election.electiondate = parse_date(self.app.pargs.date[0])
+            #     return fn(self)
+            # except ValueError:
+            #     puts(colored.yellow('Whoa there, friend! There was an error:\n'))
+            #     puts('{0} could not be recognized as a date.\n'.format(colored.green(self.app.pargs.date[0])))
+            # except KeyError:
+            #     puts(colored.yellow('Whoa there, friend! There was an error:\n'))
+            #     puts('You have not exported {0} as an environment variable.\n'.format(colored.green("AP_API_KEY")))
         elif self.app.pargs.data_file:
             self.app.election.electiondate = 'data file: {0}'.format(self.app.pargs.data_file)
             return fn(self)
