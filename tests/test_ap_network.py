5a07c3e045ef1c51db1575d94de0f6a4a5fa71d8
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-12-03 13:48:54 -0500

    Did you know you can skip tests if conditions aren't met? Closes #127.

--- tests/test_ap_network.py
+++ tests/test_ap_network.py
@@ -1,10 +1,14 @@
 import json
+import os
 import unittest
 
 from elex.parser import utils
 
 class APNetworkTestCase(unittest.TestCase):
 
+    message = "We require that you export AP_API_KEY in your environment in order to test AP connectivity."
+
+    @unittest.skipUnless(os.environ.get('AP_API_KEY', None), message)
     def test_bad_date(self):
         r = utils.api_request('/9999-99-99')
         self.assertTrue('errorCode' in r)
@@ -13,11 +17,13 @@ class APNetworkTestCase(unittest.TestCase):
         self.assertEqual(r['errorMessage'], 'String was not recognized as a valid DateTime.')
         self.assertFalse('races' in r)
 
+    @unittest.skipUnless(os.environ.get('AP_API_KEY', None), message)
     def test_nonexistent_date(self):
         r = utils.api_request('/1965-01-01')
         self.assertTrue('races' in r)
         self.assertEqual(len(r['races']), 0)
 
+    @unittest.skipUnless(os.environ.get('AP_API_KEY', None), message)
     def test_nonexistent_param(self):
         r = utils.api_request('/', foo='bar')
         self.assertTrue('errorCode' in r)
