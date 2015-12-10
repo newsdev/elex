c21c131a71f3b6b031277cace290a36cf74c3bd9
David Eads <davideads@gmail.com>
2015-12-10 01:42:28 -0500

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name='elex',
-    version='0.1.0',
+    version='0.1.1',
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/elex',
