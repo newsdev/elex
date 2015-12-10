711c3fe035db39ecf4e797d50e0f6edcddd377b0
David Eads <davideads@gmail.com>
2015-12-10 01:36:27 -0500

    add correct python version classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,10 @@ setup(
     install_requires=reqs,
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
                  'Programming Language :: Python',
-                 'Topic :: Software Development :: Libraries :: Python Modules']
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.5',
+                 ]
 )
