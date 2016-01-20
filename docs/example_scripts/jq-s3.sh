f45146a8d37e081db592fd5498a17b8edff1032c
David Eads <davideads@gmail.com>
2016-01-20 01:49:21 -0500

    add recipe section with jq-s3 recipe

---
+++ docs/example_scripts/jq-s3.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# S3 url: MUST be set to your bucket and path.
+ELEX_S3_URL='mybucket.tld/output/path.json'
+
+# Get results and upload to S3
+elex results 2012-11-06 -o json \
+| jq -c '[
+            .[] |
+            select(.level == "state" ) |
+            select(.officename == "President") |
+            {
+              officename: .officename,
+              statepostal: .statepostal,
+              first: .first,
+              last: .last,
+              party: .party,
+              votecount: .votecount,
+              votepct: .votepct,
+              winner: .winner,
+              level: .level
+            }
+         ]' \
+| gzip -vc \
+| aws s3 cp - s3://$ELEX_S3_URL \
+    --acl public-read \
+    --content-type=application/json \
+    --content-encoding gzip
+
+# Check response headers
+curl -I $ELEX_S3_URL
+
+# Get first entry of uploaded json
+curl -s --compressed $ELEX_S3_URL | jq '[.[]][0]'
