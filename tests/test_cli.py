4ae7bce040cbe6c9d61da4eeb23d3d6a870aff95
David Eads <davideads@gmail.com>
2016-01-22 02:09:40 -0500

    clean up cli test naming and add delegate tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,6 +13,8 @@ from six import with_metaclass
 
 DATA_FILE = 'tests/data/20151103_national.json'
 DATA_ELECTION_DATE = '2015-11-03'
+DELSUM_DATA_FILE = 'tests/data/20160118_delsum.json'
+DELSUPER_DATA_FILE = 'tests/data/20160118_delsuper.json'
 ELECTIONS_DATA_FILE = 'tests/data/00000000_elections.json'
 
 TEST_COMMANDS = [
@@ -62,11 +64,11 @@ class ElexCLICSVTestMeta(type):
             return test
 
         for command in TEST_COMMANDS:
-            fields_test_name = 'test_{0}_fields'.format(command.replace('-', '_'))
+            fields_test_name = 'test_csv_{0}_fields'.format(command.replace('-', '_'))
             dict[fields_test_name] = gen_fields_test(command)
-            length_test_name = 'test_{0}_length'.format(command.replace('-', '_'))
+            length_test_name = 'test_csv_{0}_length'.format(command.replace('-', '_'))
             dict[length_test_name] = gen_length_test(command)
-            data_test_name = 'test_{0}_data'.format(command.replace('-', '_'))
+            data_test_name = 'test_csv_{0}_data'.format(command.replace('-', '_'))
             dict[data_test_name] = gen_data_test(command)
 
         return type.__new__(mcs, name, bases, dict)
@@ -80,47 +82,56 @@ class ElexCLICSVTestCase(with_metaclass(ElexCLICSVTestMeta, tests.ElectionResult
     Python API. The API tests guarantee the validity of the data, while these
     tests guarantee the CLI provides the same data in CSV format.
     """
-    def test_elections_fields(self):
+    def test_csv_elections_fields(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(fields, ['id', 'electiondate', 'liveresults', 'testresults'])
 
-    def test_elections_length(self):
+    def test_csv_elections_length(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(len(data), 11)
 
-    def test_elections_date(self):
+    def test_csv_elections_date(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['electiondate'], '2015-08-04')
 
-    def test_elections_liveresults(self):
+    def test_csv_elections_liveresults(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['liveresults'], 'False')
 
-    def test_elections_testresults(self):
+    def test_csv_elections_testresults(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['testresults'], 'True')
 
-    def test_next_election_fields(self):
+    def test_csv_next_election_fields(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(fields, ['id', 'electiondate', 'liveresults', 'testresults'])
 
-    def test_next_election_length(self):
+    def test_csv_next_election_length(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(len(data), 1)
 
-    def test_next_election_date(self):
+    def test_csv_next_election_date(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['electiondate'], '2015-08-25')
 
-    def test_next_election_liveresults(self):
+    def test_csv_next_election_liveresults(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['liveresults'], 'True')
 
-    def test_next_election_testresults(self):
+    def test_csv_next_election_testresults(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['testresults'], 'False')
 
-    def _test_command(self, command, datafile=DATA_FILE, electiondate=DATA_ELECTION_DATE):
+    def test_csv_delegate_fields(self):
+        fields, data = self._test_command(command='delegates')
+        self.assertEqual(fields, ['level', 'party_total','superdelegates_count', 'last', 'state', 'candidateid', 'party_need','party', 'delegates_count', 'id'])
+
+    def test_csv_delegate_state_count(self):
+        fields, data = self._test_command(command='delegates')
+        number_of_states = list(set([d['state'] for d in data if d['level'] == 'state']))
+        self.assertEqual(58, len(number_of_states))
+
+    def _test_command(self, command, datafile=DATA_FILE, delsum_datafile=DELSUM_DATA_FILE, delsuper_datafile=DELSUPER_DATA_FILE, electiondate=DATA_ELECTION_DATE):
         """
         Execute an `elex` sub-command; returns fieldnames and rows
         """
@@ -133,6 +144,8 @@ class ElexCLICSVTestCase(with_metaclass(ElexCLICSVTestMeta, tests.ElectionResult
             argv.append(electiondate)
 
         argv = argv + ['--data-file', datafile]
+        argv = argv + ['--delegate-sum-file', delsum_datafile]
+        argv = argv + ['--delegate-super-file', delsuper_datafile]
 
         app = ElexApp(argv=argv)
 
@@ -185,11 +198,11 @@ class ElexCLIJSONTestMeta(type):
             return test
 
         for command in TEST_COMMANDS:
-            fields_test_name = 'test_{0}_fields'.format(command.replace('-', '_'))
+            fields_test_name = 'test_json_{0}_fields'.format(command.replace('-', '_'))
             dict[fields_test_name] = gen_fields_test(command)
-            length_test_name = 'test_{0}_length'.format(command.replace('-', '_'))
+            length_test_name = 'test_json_{0}_length'.format(command.replace('-', '_'))
             dict[length_test_name] = gen_length_test(command)
-            data_test_name = 'test_{0}_data'.format(command.replace('-', '_'))
+            data_test_name = 'test_json_{0}_data'.format(command.replace('-', '_'))
             dict[data_test_name] = gen_data_test(command)
 
         return type.__new__(mcs, name, bases, dict)
@@ -203,47 +216,56 @@ class ElexCLIJSONTestCase(with_metaclass(ElexCLIJSONTestMeta, tests.ElectionResu
     Python API. The API tests guarantee the validity of the data, while these
     tests guarantee the CLI provides the same data in JSON format.
     """
-    def test_elections_fields(self):
+    def test_json_elections_fields(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(fields, ['id', 'electiondate', 'liveresults', 'testresults'])
 
-    def test_elections_length(self):
+    def test_json_elections_length(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(len(data), 11)
 
-    def test_elections_date(self):
+    def test_json_elections_date(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['electiondate'], '2015-08-04')
 
-    def test_elections_liveresults(self):
+    def test_json_elections_liveresults(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['liveresults'], False)
 
-    def test_elections_testresults(self):
+    def test_json_elections_testresults(self):
         fields, data = self._test_command(command='elections', datafile=ELECTIONS_DATA_FILE)
         self.assertEqual(data[4]['testresults'], True)
 
-    def test_next_election_fields(self):
+    def test_json_next_election_fields(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(fields, ['id', 'electiondate', 'liveresults', 'testresults'])
 
-    def test_next_election_length(self):
+    def test_json_next_election_length(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(len(data), 1)
 
-    def test_next_election_date(self):
+    def test_json_next_election_date(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['electiondate'], '2015-08-25')
 
-    def test_next_election_liveresults(self):
+    def test_json_next_election_liveresults(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['liveresults'], True)
 
-    def test_next_election_testresults(self):
+    def test_json_next_election_testresults(self):
         fields, data = self._test_command(command='next-election', datafile=ELECTIONS_DATA_FILE, electiondate='2015-08-04')
         self.assertEqual(data[0]['testresults'], False)
 
-    def _test_command(self, command, datafile=DATA_FILE, electiondate=DATA_ELECTION_DATE):
+    def test_json_delegate_fields(self):
+        fields, data = self._test_command(command='delegates')
+        self.assertEqual(fields, ['level', 'party_total','superdelegates_count', 'last', 'state', 'candidateid', 'party_need','party', 'delegates_count', 'id'])
+
+    def test_json_delegate_state_count(self):
+        fields, data = self._test_command(command='delegates')
+        number_of_states = list(set([d['state'] for d in data if d['level'] == 'state']))
+        self.assertEqual(58, len(number_of_states))
+
+    def _test_command(self, command, datafile=DATA_FILE, delsum_datafile=DELSUM_DATA_FILE, delsuper_datafile=DELSUPER_DATA_FILE, electiondate=DATA_ELECTION_DATE):
         """
         Execute an `elex` sub-command; returns fieldnames and rows
         """
@@ -255,6 +277,8 @@ class ElexCLIJSONTestCase(with_metaclass(ElexCLIJSONTestMeta, tests.ElectionResu
         argv.append(electiondate)
 
         argv = argv + ['--data-file', datafile, '-o', 'json']
+        argv = argv + ['--delegate-sum-file', delsum_datafile]
+        argv = argv + ['--delegate-super-file', delsuper_datafile]
 
         app = ElexApp(argv=argv)
 
