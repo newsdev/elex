e8f4b2dbb86d34a6fe3edff6f7de4ba81d126650
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-02-25 15:04:05 -0500

    1.2.1b1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename):
 
 setup(
     name='elex',
-    version='1.2.1',
+    version='1.2.1b1',
     author='Jeremy Bowers, David Eads',
     author_email='jeremy.bowers@nytimes.com, deads@npr.org',
     url='https://github.com/newsdev/elex',
