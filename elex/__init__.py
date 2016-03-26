0ae2f2a49e103d3be3bd689876bfb59140c9f658
David Eads <davideads@gmail.com>
2016-03-26 00:39:05 -0400

    nicer formatted base url

--- elex/__init__.py
+++ elex/__init__.py
@@ -4,5 +4,5 @@ import pkg_resources
 
 __version__ = pkg_resources.get_distribution('elex').version
 VERSION = os.environ.get('AP_API_VERSION', 'v2')
-BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/%s' % VERSION)
+BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/{0}'.format(VERSION))
 API_KEY = os.environ.get('AP_API_KEY', None)
