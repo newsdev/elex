f6869209d810dcede12fdd1b3e59323cfbc26a4c
David Eads <davideads@gmail.com>
2016-03-27 15:50:05 -0400

    change constant name to be more specific

--- elex/__init__.py
+++ elex/__init__.py
@@ -3,6 +3,6 @@ import pkg_resources
 
 
 __version__ = pkg_resources.get_distribution('elex').version
-VERSION = os.environ.get('AP_API_VERSION', 'v2')
-BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/{0}'.format(VERSION))
+API_VERSION = os.environ.get('AP_API_VERSION', 'v2')
+BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/{0}'.format(API_VERSION))
 API_KEY = os.environ.get('AP_API_KEY', None)
