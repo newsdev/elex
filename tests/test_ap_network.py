4c06d4721e5217bb0a2d46ffa85c431ef2ac4172
David Eads <davideads@gmail.com>
2016-04-10 21:35:17 -0400

    refactor network tests to match new exception based system, skip if over quota limit

--- tests/test_ap_network.py
+++ tests/test_ap_network.py
@@ -1,22 +1,33 @@
 import os
 import unittest
 
+from requests.exceptions import HTTPError
 from time import sleep
 from . import NetworkTestCase, API_MESSAGE
 
 
 class APNetworkTestCase(NetworkTestCase):
 
+    def tearDown(self, **kwargs):
+        sleep(5)
+
     @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
     def test_bad_date_error_code(self):
-        sleep(5)
-        self.assertEqual(self.api_request('/elections/9999-99-99').status_code, 400)
+        with self.assertRaises(HTTPError) as error:
+            self.api_request('/elections/9999-99-99')
+        response = error.exception.response
+        if response.status_code == 403:
+            self.skipTest('over quota limit')
+        self.assertEqual(response.status_code, 400)
 
     @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
     def test_bad_date_error_message(self):
-        sleep(5)
-        bad_date_response = self.api_request('/elections/9999-99-99')
-        data = bad_date_response.json()
+        with self.assertRaises(HTTPError) as error:
+            self.api_request('/elections/9999-99-99')
+        response = error.exception.response
+        if response.status_code == 403:
+            self.skipTest('over quota limit')
+        data = response.json()
         self.assertEqual(
             data['errorMessage'],
             'String was not recognized as a valid DateTime.'
@@ -24,21 +35,25 @@ class APNetworkTestCase(NetworkTestCase):
 
     @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
     def test_bad_date_fields(self):
-        sleep(5)
-        bad_date_response = self.api_request('/elections/9999-99-99')
-        data = bad_date_response.json()
-        keys = data.keys()
+        with self.assertRaises(HTTPError) as error:
+            self.api_request('/elections/9999-99-99')
+        response = error.exception.response
+        if response.status_code == 403:
+            self.skipTest('over quota limit')
+        keys = response.json().keys()
         self.assertTrue('errorMessage' in keys and 'errorCode' in keys)
 
     @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
     def test_nonexistent_date(self):
-        sleep(5)
         nonexistent_date_response = self.api_request('/elections/1965-01-01')
         data = nonexistent_date_response.json()
         self.assertEqual(len(data['races']), 0)
 
     @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
     def test_nonexistent_param(self):
-        sleep(5)
-        nonexistent_param_response = self.api_request('/elections/', foo='bar')
-        self.assertEqual(nonexistent_param_response.status_code, 400)
+        with self.assertRaises(HTTPError) as error:
+            self.api_request('/elections/', foo='bar')
+        response = error.exception.response
+        if response.status_code == 403:
+            self.skipTest('over quota limit')
+        self.assertEqual(response.status_code, 400)
