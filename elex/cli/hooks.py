fbfd3389fbb1cf594bfa0a2a828323230c01bf96
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-04-21 10:35:15 -0400

    Adds a --national-true flag. Properly assigns national keyword to app.election based on presence of command-line flags.

--- elex/cli/hooks.py
+++ elex/cli/hooks.py
@@ -5,13 +5,20 @@ def add_election_hook(app):
     """
     Cache election API object reference after parsing args.
     """
+
     app.election = Election(
         testresults=app.pargs.test,
         liveresults=not app.pargs.not_live,
         resultslevel=app.pargs.results_level,
         setzerocounts=app.pargs.set_zero_counts,
-        national=app.pargs.local_only,
         is_test=False
     )
+
     if app.pargs.data_file:
         app.election.datafile = app.pargs.data_file
+
+    if app.pargs.national_only:
+        app.election.national = True
+
+    if app.pargs.local_only:
+        app.election.national = False
