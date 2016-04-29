e37bede4379bd4c36156c1d3fb04da2e153519be
David Eads <davideads@gmail.com>
2016-04-29 11:55:10 -0400

    add gzip header to default session

--- elex/__init__.py
+++ elex/__init__.py
@@ -11,5 +11,8 @@ API_VERSION = os.environ.get('AP_API_VERSION', 'v2')
 BASE_URL = os.environ.get('AP_API_BASE_URL', 'http://api.ap.org/{0}'.format(API_VERSION))
 API_KEY = os.environ.get('AP_API_KEY', None)
 CACHE_DIRECTORY = os.path.join(tempfile.gettempdir(), 'elex-cache')
-cache = CacheControl(requests.session(),
+
+session = requests.session()
+session.headers.update({'Accept-Encoding': 'gzip'})
+cache = CacheControl(session,
                      cache=FileCache(CACHE_DIRECTORY))
