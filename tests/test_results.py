14b1056133ac847fc466f9a18a24caf9405e892c
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-08-31 14:44:26 -0400

    Okay, need a more fail-y test.

--- tests/test_results.py
+++ tests/test_results.py
@@ -7,77 +7,96 @@ from elex.api import maps
 import tests
 
 
-class TestPrecinctsReportingPctFloat(tests.ElectionResultsTestCase):
-    data_url = 'tests/data/20160301_super_tuesday.json'
+# class TestPrecinctsReportingPctFloat(tests.ElectionResultsTestCase):
+#     data_url = 'tests/data/20160301_super_tuesday.json'
 
-    def test_precincts_reporting_pct_less_than_one_point_oh(self):
-        results = [r for r in self.results]
-        for r in results:
-            percent = float(r.precinctsreporting) / float(r.precinctstotal)
-            self.assertEqual(
-                "%.4f" % r.precinctsreportingpct,
-                "%.4f" % percent
-            )
-            self.assertLessEqual(r.precinctsreportingpct, 1.00)
-            self.assertGreaterEqual(r.precinctsreportingpct, 0.00)
-
-
-class TestMassRollupBug(tests.ElectionResultsTestCase):
-    """
-    Adding up all of the level "township" should equal
-    the totals for "county" but that's not true for
-    Nantucket county, MA and the townships in fips 25019.
-    """
-    data_url = 'tests/data/20160301_super_tuesday.json'
-
-    def test_number_of_counties(self):
-        """
-        According to bug #236, we should be 1 county short.
-        """
-        mass_results = [
-            r for r in self.results if
-            r.raceid == '24547' and
-            r.level == 'county' and
-            r.last == 'Trump'
-        ]
-        self.assertEqual(len(mass_results), len(maps.FIPS_TO_STATE['MA']))
+#     def test_precincts_reporting_pct_less_than_one_point_oh(self):
+#         results = [r for r in self.results]
+#         for r in results:
+#             percent = float(r.precinctsreporting) / float(r.precinctstotal)
+#             self.assertEqual(
+#                 "%.4f" % r.precinctsreportingpct,
+#                 "%.4f" % percent
+#             )
+#             self.assertLessEqual(r.precinctsreportingpct, 1.00)
+#             self.assertGreaterEqual(r.precinctsreportingpct, 0.00)
 
 
-class TestElectionDateSuperTuesday(tests.ElectionResultsTestCase):
-    """
-    When using data files, election date should be automatically inferred.
-    """
-    data_url = 'tests/data/20160301_super_tuesday.json'
+# class TestMassRollupBug(tests.ElectionResultsTestCase):
+#     """
+#     Adding up all of the level "township" should equal
+#     the totals for "county" but that's not true for
+#     Nantucket county, MA and the townships in fips 25019.
+#     """
+#     data_url = 'tests/data/20160301_super_tuesday.json'
 
-    def test_supertuesday_electiondate(self):
-        self.assertEqual(self.election.electiondate, '2016-03-01')
+#     def test_number_of_counties(self):
+#         """
+#         According to bug #236, we should be 1 county short.
+#         """
+#         mass_results = [
+#             r for r in self.results if
+#             r.raceid == '24547' and
+#             r.level == 'county' and
+#             r.last == 'Trump'
+#         ]
+#         self.assertEqual(len(mass_results), len(maps.FIPS_TO_STATE['MA']))
 
 
-class TestElectionDate2015(tests.ElectionResultsTestCase):
-    """
-    When using data files, election date should be automatically inferred.
-    """
-    data_url = 'tests/data/20151103_national.json'
+# class TestElectionDateSuperTuesday(tests.ElectionResultsTestCase):
+#     """
+#     When using data files, election date should be automatically inferred.
+#     """
+#     data_url = 'tests/data/20160301_super_tuesday.json'
 
-    def test_2015_electiondate(self):
-        self.assertEqual(self.election.electiondate, '2015-11-03')
+#     def test_supertuesday_electiondate(self):
+#         self.assertEqual(self.election.electiondate, '2016-03-01')
+
+
+# class TestElectionDate2015(tests.ElectionResultsTestCase):
+#     """
+#     When using data files, election date should be automatically inferred.
+#     """
+#     data_url = 'tests/data/20151103_national.json'
+
+#     def test_2015_electiondate(self):
+#         self.assertEqual(self.election.electiondate, '2015-11-03')
 
 
 class TestGeneralElectionEdgeCases(tests.ElectionResultsTestCase):
+    data_url = 'tests/data/20121106_national.json'
+
     """
     There should be 51 unique state reportingunit ids, not 1.
     """
-    data_url = 'tests/data/20121106_national.json'
-
     def test_general_stateids(self):
         state_results = [r.reportingunitid for r in self.reporting_units if r.level == 'state']
         unique_state_results = list(set(state_results))
         self.assertEqual(len(state_results), len(unique_state_results))
 
-    def test_electwon(self):
-        arbitrary_cru = [r for r in self.reporting_units if r.officeid == 'P' and r.level == 'state'][0]
-        self.assertTrue(hasattr(arbitrary_cru, 'electwon'))
+    """
+    There should be some fields for electoral votes.
+    """
+    def test_electwon_exists_cru(self):
+        r = [r for r in self.results if r.officeid == 'P' and r.level == 'state'][0]
+        self.assertTrue(hasattr(r, 'electwon'))
 
-    def test_electtotal(self):
-        arbitrary_cru = [r for r in self.reporting_units if r.officeid == 'P' and r.level == 'state'][0]
-        self.assertTrue(hasattr(arbitrary_cru, 'electtotal'))
+    def test_electtotal_exists_cru(self):
+        r = [r for r in self.results if r.officeid == 'P' and r.level == 'state'][0]
+        self.assertTrue(hasattr(r, 'electtotal'))
+
+    def test_electtotal_exists_ru(self):
+        r = [r for r in self.reporting_units if r.officeid == 'P' and r.level == 'state'][0]
+        self.assertTrue(hasattr(r, 'electtotal'))
+
+    """
+    Obama won all 3 of AK's electoral votes in 2012.
+    """
+
+    def test_electwon_is_set(self):
+        r = [r for r in self.results if r.officeid == 'P' and r.level == 'state' and r.last == 'Obama' and r.statepostal == 'AK']
+        self.assertEqual(r[0].electwon, 3)
+
+    def test_electtotal_is_set(self):
+        r = [r for r in self.results if r.officeid == 'P' and r.level == 'state' and r.last == 'Obama' and r.statepostal == 'AK']
+        self.assertEqual(r[0].electtotal, 3)
