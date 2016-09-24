d6cba1efffd663b89aa81566cafbac1b4da19e97
David Eads <davideads@gmail.com>
2016-09-24 00:15:32 -0400

    add cache test cases

--- tests/test_ap_network.py
+++ tests/test_ap_network.py
@@ -34,3 +34,24 @@ class APNetworkTestCase(NetworkTestCase):
         if response.status_code == 403:
             self.skipTest('over quota limit')
         self.assertEqual(response.status_code, 400)
+
+
+class ElexNetworkCacheTestCase(NetworkTestCase):
+
+    @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
+    def test_elex_cache_miss(self):
+        from elex import cache
+        response = self.api_request('/elections/2016-02-01')
+        http_adapter = cache.get_adapter('http://')
+        try:
+            http_adapter.cache.delete(response.url)
+        except OSError:
+            pass
+        uncached_response = self.api_request('/elections/2016-02-01')
+        self.assertEqual(uncached_response.from_cache, False)
+
+    @unittest.skipUnless(os.environ.get('AP_API_KEY', None), API_MESSAGE)
+    def test_elex_cache_hit(self):
+        self.api_request('/elections/2016-02-01')
+        cached_response = self.api_request('/elections/2016-02-01')
+        self.assertEqual(cached_response.from_cache, True)
