eccc5ccae53ea2eb926cb67b940c2dd372b680e4
Lewis <matt@csiga.org>
2018-11-24 17:25:14 -0600

    Typo correction: Politian -> Politician

--- elex/api/models.py
+++ elex/api/models.py
@@ -255,7 +255,7 @@ class BallotMeasure(APElection):
         Generate and set unique id.
 
         Candidate IDs are not globally unique.
-        AP National Politian IDs (NPIDs or polid)
+        AP National Politician IDs (NPIDs or polid)
         are unique, but only national-level
         candidates have them; everyone else gets '0'.
         The unique key, then, is the NAME of the ID
@@ -361,7 +361,7 @@ class CandidateReportingUnit(APElection):
         Generate and set unique id.
 
         Candidate IDs are not globally unique.
-        AP National Politian IDs (NPIDs or polid)
+        AP National Politician IDs (NPIDs or polid)
         are unique, but only national-level
         candidates have them; everyone else gets '0'.
         The unique key, then, is the NAME of the ID
