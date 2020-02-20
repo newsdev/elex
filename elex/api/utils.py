00e0ce95a4936a34d1826e2844394b05f0b29286
james@jwilkerson.me <james@jwilkerson.me>
2020-02-20 14:39:32 -0600

    Added numrunoff to api query

--- elex/api/utils.py
+++ elex/api/utils.py
@@ -84,6 +84,7 @@ def api_request(path, **params):
         `apiKey="<YOUR API KEY>`, your AP API key, or `national=True`,
         for national-only results.
     """
+
     params['apiKey'] = params.get('apiKey') or elex.API_KEY
     if not params['apiKey']:
         raise APAPIKeyException()
@@ -94,8 +95,8 @@ def api_request(path, **params):
 
     url = '{0}{1}'.format(elex.BASE_URL, path.replace('//', '/'))
     response = cache.get(url, params=params)
+    # import ipdb; ipdb.set_trace()
     response.raise_for_status()
-
     write_recording(response.json())
     return response
 
